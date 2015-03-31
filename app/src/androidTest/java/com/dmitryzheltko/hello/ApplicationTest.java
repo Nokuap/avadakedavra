eed27aa06296fc3e35c823c8126da212fa069058
unknown <dmitry.zheltko@WKS-089.hiqo-solutions.com>
2015-03-31 17:52:21 +0300

    123

--- app/src/androidTest/java/com/dmitryzheltko/hello/ApplicationTest.java
+++ app/src/androidTest/java/com/dmitryzheltko/hello/ApplicationTest.java
@@ -1,4 +1,4 @@
-package com.dmitryzheltko.df;
+package com.dmitryzheltko.hello;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
---
+++ app/src/main/java/com/dmitryzheltko/hello/hello.java
@@ -0,0 +1,10 @@
+package com.dmitryzheltko.hello;
+
+/**
+ * Created by dmitry.zheltko on 3/31/2015.
+ */
+public class hello {
+    public static void hello(){
+
+    }
+}
